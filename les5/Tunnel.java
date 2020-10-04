9768d452e10daaf6432e0d41a28be8319190ec0c
thermo911 <thermo911@yandex.ru>
2020-10-04 16:06:17 +0300

    lesson 5 work done

---
+++ les5/Tunnel.java
@@ -0,0 +1,28 @@
+package les5;
+
+public class Tunnel extends Stage {
+    public Tunnel() {
+        this.length = 80;
+        this.description = "Тоннель " + length + " метров";
+    }
+
+    @Override
+    public void go(Car c) {
+        try {
+            try {
+                System.out.println(c.getName() + " готовится к этапу(ждет): " + description);
+                c.acquire();
+                System.out.println(c.getName() + " начал этап: " + description);
+                Thread.sleep(length / c.getSpeed() * 1000);
+
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                System.out.println(c.getName() + " закончил этап: " + description);
+                c.release();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
